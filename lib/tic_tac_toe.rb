8c2b1b5573893f0f79a8ef618f0283589b61a247
Ashleigh Chun <ashleigh.chun@wework.com>
2018-08-31 02:17:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,8 +13,8 @@ WIN_COMBINATIONS = [
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
-def display_board=(board)
-    @board = board
+  def display_board
+    @board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
