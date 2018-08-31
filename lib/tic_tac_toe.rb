100cce897c37ea68f0bcb7249a0b47299cb2e93e
Ashleigh Chun <ashleigh.chun@wework.com>
2018-08-31 02:49:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,9 @@ end
 def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 end
 
